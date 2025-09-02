fca2f29e8054c1d6e225aa08c0c7ebb07e88fad1
Errachdi <errachdi.og@gmail.com>
2025-09-02 10:39:16 +0100

    add logo icon

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet, NavLink } from 'react-router';
+import logo from '../assets/logo.png'
 
 export default function Layout() {
   return (
     <div className="min-h-screen bg-slate-50">
       <header className="fixed top-0 left-0 right-0 h-16 bg-white shadow z-30 flex items-center px-4">
-        <div className="font-bold mr-6">YourLogo</div>
-        <nav className="flex gap-3">
+        <img src={logo} alt="Logo" className='size-8'/>
+        <nav className="flex gap-4 ml-5">
           <NavLink to="/" className={({isActive}) => isActive ? 'text-indigo-600 font-bold' : 'text-slate-700 font-bold'}>Home</NavLink>
           <NavLink to="/editor" className={({isActive}) => isActive ? 'text-indigo-600 font-bold' : 'text-slate-700 font-bold'}>Editor</NavLink>
         </nav>
